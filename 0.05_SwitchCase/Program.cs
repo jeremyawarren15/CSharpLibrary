545ef199bbf89fc274cba6f9979fb46cea75fd23
Jeremy Warren <jeremyawarren15@gmail.com>
2017-10-25 14:26:29 -0400

    added swtich case project

---
+++ 0.05_SwitchCase/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._05_SwitchCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string friend = "Jeremy";
+
+            switch (friend)
+            {
+                case "Jeremy":
+                    Console.WriteLine("Hey Jeremy");
+                    break;
+                case "Paul":
+                    Console.WriteLine("Paul is pretty cool");
+                    break;
+                default:
+                    Console.WriteLine("I don't know you");
+                    break;
+            }
+        }
+    }
+}
