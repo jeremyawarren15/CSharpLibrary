bf2617dca3a3a00e474afa468ccfa1ec0ed17273
Jeremy Warren <jeremyawarren15@gmail.com>
2017-10-25 11:15:14 -0400

    Added initial files

---
+++ 0.002_BasicTypes/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._002_BasicTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 15;
+            int number2 = 5;
+            int number3;
+
+            byte myByte = 255;
+
+            char myLetter = 'A';
+            string myName = "Jeremy";
+
+            float myFloat = 3.14f;
+
+            double myDouble = 2.54;
+
+            bool myBool = false;
+
+            Console.WriteLine(myName);
+            Console.Read();
+
+            // Declaration
+            // <type> <variableName> = <optional initializer>;
+            int age;
+            string country;
+
+            // Initialization
+            age = 25;
+            Console.WriteLine(age);
+            age = 26;
+            Console.WriteLine(age);
+
+        }
+    }
+}
---
+++ 0.02_BasicTypesChallenges/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._02_BasicTypesChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Bronze Challenge
+            int myInt = 1;
+            string myString;
+            bool myBool = true;
+            float myFloat = 3.14f;
+            double myDouble = 9.23423;
+            decimal myDecimal = 1234123.234M;
+
+            // Silver Challenge
+            myString = $"{myInt} {myBool} {myFloat} {myDouble} {myDecimal}";
+            Console.WriteLine(myString);
+
+            // Gold Challenge
+            string year = "1991";
+            Console.WriteLine($"I was born in {Int32.Parse(year)}");
+            Console.Read();
+        }
+    }
+}
