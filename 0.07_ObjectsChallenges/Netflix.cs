e9172657154062ebc8edeb4e67bb8d01df8da268
Jeremy Warren <jeremyawarren15@gmail.com>
2017-10-26 11:08:54 -0400

    added project challenges for objects

---
+++ 0.07_ObjectsChallenges/Netflix.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._07_ObjectsChallenges
+{
+    class Netflix
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+
+        public void GetSuggestion()
+        {
+            if (Rating >= 4)
+            {
+                Console.WriteLine("I would watch this movie.");
+            }
+            else
+            {
+                Console.WriteLine("I wouldn't watch this movie.");
+            }
+        }
+    }
+}
---
+++ 0.07_ObjectsChallenges/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._07_ObjectsChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Netflix theOffice = new Netflix();
+            Netflix strangerThings = new Netflix();
+            Netflix lost = new Netflix();
+
+            theOffice.Name = "The Office";
+            theOffice.Genre = "Comedy";
+            theOffice.Rating = 3;
+
+            strangerThings.Name = "Stranger Things";
+            strangerThings.Genre = "Thriller";
+            strangerThings.Rating = 5;
+
+            lost.Name = "Lost";
+            lost.Genre = "Thriller";
+            lost.Rating = 4;
+
+            List<Netflix> favoriteShows = new List<Netflix>();
+
+            favoriteShows.Add(theOffice);
+            favoriteShows.Add(strangerThings);
+            favoriteShows.Add(lost);
+
+            foreach (Netflix show in favoriteShows)
+            {
+                Console.WriteLine(show.Name);
+            }
+        }
+    }
+}
