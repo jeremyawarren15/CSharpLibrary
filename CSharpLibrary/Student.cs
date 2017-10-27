87cda76715581d448ab67b292e398b2cefcc2d35
Jeremy Warren <jeremyawarren15@gmail.com>
2017-10-27 09:39:08 -0400

    added project on constructors

---
+++ CSharpLibrary/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpLibrary
+{
+    class Student
+    {
+        // Constructors
+        public Student(string firstName, string lastName, int age, int grade)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Grade = grade;
+        }
+
+        // Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int Grade { get; set; }
+        
+        
+    }
+}
