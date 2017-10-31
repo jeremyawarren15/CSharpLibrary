57a74ec90af852519a418545cb892c56b9fba9fe
Jeremy Warren <jeremyawarren15@gmail.com>
2017-10-31 08:26:07 -0400

    added an enemy, character, and player class with some limited functionality

---
+++ 0.10_OOPGame/Character.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._10_OOPGame
+{
+    public abstract class Character
+    {
+        // Constants
+        const int Min_Helth = 0;
+
+        // Properties
+        public string Name { get; set; }
+        public bool IsAlive { get; set; }
+        public int Level { get; set; }
+        public int Health { get; set; }
+        public int AttackLevel { get; set; }
+        public int Stamina { get; set; }
+        public int Luck { get; set; }
+        
+        public void GetPowerLevel()
+        {
+            Random rnd = new Random();
+            this.Level =  rnd.Next(1, 100);
+        }
+
+        public int Attack(int attackLevel)
+        {
+            Random rnd = new Random();
+            return rnd.Next(1, attackLevel);
+        }
+    }
+}
--- 0.10_OOPGame/Program.cs
+++ 0.10_OOPGame/Program.cs
@@ -10,6 +10,21 @@ namespace _0._10_OOPGame
     {
         static void Main(string[] args)
         {
+            Player newHero = new Player();
+
+            Console.Write("Greetings, wayfarer! What be thy name? ");
+            newHero.Name = Console.ReadLine();
+
+            Console.Clear();
+            Console.Write($"Nice to meet you, {newHero.Name}.\n");
+
+            Console.Write($"What's your specialization frield?" +
+                $"1: Horse Mange\n" +
+                $"2: Troll Cat\n" +
+                $"3: Knight Templatetor\n" +
+                $"4: Demogorg\n" +
+                $"5: Sire Cena\n" +
+                $"Your choice: ");
         }
     }
 }
