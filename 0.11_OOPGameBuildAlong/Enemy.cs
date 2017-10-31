aff961ae69df7ff957429f12c703d1137db9177b
Jeremy Warren <jeremyawarren15@gmail.com>
2017-10-31 13:22:41 -0400

    made a lot of changes...

--- 0.11_OOPGameBuildAlong/Enemy.cs
+++ 0.11_OOPGameBuildAlong/Enemy.cs
@@ -8,5 +8,17 @@ namespace _0._11_OOPGameBuildAlong
 {
     class Enemy: Character
     {
+        //Constructor
+        public Enemy(int level)
+        {
+            this.Name = "CSharpVirus";
+            this.Level = level;
+            this.IsAlive = true;
+            this.Health = 100;
+            this.AttackPower = 12;
+            this.CritChance = 0.10;
+        }
+        //Properties
+        public string Name { get; set; }
     }
 }
