11bf48313187a80048bbed5a560f516816cded63
Jeremy Warren <jeremyawarren15@gmail.com>
2017-11-02 09:33:55 -0400

    added solution to part one

--- 0.16_QuizOne/Program.cs
+++ 0.16_QuizOne/Program.cs
@@ -10,6 +10,17 @@ namespace _0._16_QuizOne
     {
         static void Main(string[] args)
         {
+            List<Triangle> shapes = new List<Triangle>();
+            shapes.Add(new Triangle(4, 5));
+            shapes.Add(new Triangle(3, 5));
+            shapes.Add(new Triangle(2, 8));
+            shapes.Add(new Triangle(5, 2));
+            shapes.Add(new Triangle(2, 3));
+
+            foreach (var triangle in shapes)
+            {
+                Console.WriteLine(triangle.TArea);
+            }
         }
     }
 }
---
+++ 0.16_QuizOne/Triangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._16_QuizOne
+{
+    class Triangle : Shape
+    {
+        // Constructors
+        public Triangle(int tBase, int height)
+        {
+            this.TBase = tBase;
+            this.Height = height;
+            this.TArea = this.Area();
+        }
+
+        // Properties
+        public int TBase { get; set; }
+        public int Height { get; set; }
+        public double TArea { get; set; }
+
+        // Methods
+        public override double Area()
+        {
+            this.TArea = (this.Height * this.TBase) / 2.0;
+            return this.TArea;
+        }
+    }
+}
